0ebe59bbc5c687c560ebef7dc23f91901a29fdf9
junhyeong <pson09@naver.com>
2026-03-13 17:49:29 +0900

    sixth commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     $('.hyeongflex-logo').click(function () {
         $(this).addClass('on');
+        $('body > p').hide()
         $('.browser-container .imgdummy').addClass('on');
     });
 
@@ -13,22 +14,19 @@ $(function () {
         $(".tabitem img").stop().hide();
         $(".tabitem img").eq(idx).stop().fadeIn(700);
     });
-    $(function () {
-        $(".toolbox .button > li").click(function () {
-            if ($(this).find("ul").is(":visible")) return;
-            $(".toolbox .button > li").removeClass("active");
-            $(this).addClass("active");
-            $(".toolbox .button > li ul").stop().slideUp(300);
-            $(this).find("ul").stop().slideDown(300);
-            let idx = $(this).index();
-            $(".tabitem img").stop().hide();
-            $(".tabitem img").eq(idx).stop().fadeIn(700);
-        });
-        $(".toolbox .button > li").eq(0).trigger("click");
+    $(".toolbox .button > li").click(function () {
+        if ($(this).find("ul").is(":visible")) return;
+        $(".toolbox .button > li").removeClass("active");
+        $(this).addClass("active");
+        $(".toolbox .button > li ul").stop().slideUp(300);
+        $(this).find("ul").stop().slideDown(300);
+        let idx = $(this).index();
+        $(".tabitem img").stop().hide();
+        $(".tabitem img").eq(idx).stop().fadeIn(700);
     });
     $(".toolbox .button > li").eq(0).trigger("click");
-});
-$(function () {
+
+    $(".toolbox .button > li").eq(0).trigger("click");
 
     $('header ul li').click(function () {
         $(this).addClass('on').siblings().removeClass('on')
