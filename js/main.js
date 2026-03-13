26ef36eff678fb35145be8447f483a65a2a3a509
junhyeong <pson09@naver.com>
2026-03-13 15:35:55 +0900

    fifth commit

--- js/main.js
+++ js/main.js
@@ -28,3 +28,26 @@ $(function () {
     });
     $(".toolbox .button > li").eq(0).trigger("click");
 });
+$(function () {
+
+    $('header ul li').click(function () {
+        $(this).addClass('on').siblings().removeClass('on')
+        let idx = $(this).index()
+        let sd = $('main section').eq(idx).offset().top - 114
+        $('html, body').animate({
+            scrollTop: sd
+        })
+    })
+    $(window).scroll(function () {
+        $('main section').each(function () {
+            if ($(this).offset().top <= $(window).scrollTop() + 114) {
+                let idx = $(this).index()
+                $('header ul li').removeClass('on')
+                $('header ul li').eq(idx).addClass('on')
+            }
+        })
+        // header ul li 클릭했을 때 할 일
+        // 클릭한 li의 색(on)
+        // 해당하는 section 이동
+    })
+})
